1f6a03dcb0de57cf98e7bf8e3e4825d941bacd3c
Dennis Aprilla <dennis.ap.ch@gmail.com>
2022-09-30 18:11:30 +0200

    Change Amode connection to be above Qualisys Connection. Qualisys thread somehow can't execute join() function outside the IF, so i improvise a bit (logic details is in the code)

--- src/main.cpp
+++ src/main.cpp
@@ -17,8 +17,6 @@ int main()
 	std::cout << "Qualisys + Ultrasound TCP Connection." << std::endl;
 
 	bool useQualisys = true;
-	bool useAMode    = true;
-	bool useBMode	 = false;
 
 	// initalize value for Qualisys system
 	std::string qualisys_ip = "127.0.0.1";
@@ -34,6 +32,43 @@ int main()
 	int amode_samples = 1500;
 	int amode_probes = 30;
 
+
+
+	// If not using qualisys, directly start the synch
+	// There is a backstory behind this line. The goal is to run Qualisys and AMode connection in parallel, but, i also want
+	// a feature that i can run AMode alone without using Qualisys. However, A-mode streaming is dependent to Qualisys because
+	// Qualisys is the one who control every device to start. So the situation become quite tricky. There is an elegant
+	// solution to this. That is:
+	// 1) Check if the user want to use Qualisys, if true, initiate connection and create a thread within the IF, if otherwise
+	//    start initiate synch::start() to allow other device to start.
+	// 2) After the if block, initiate Amode connection and create a thread.
+	// 3) When user is finished by pressing ESQ, the threads will be finished, and it will by catched by .join() function.
+	// 
+	// Since the qualisys thread will be created inside the IF block, so we need to declare the thread (without doing anything)
+	// before the if. Everything supposed to be going well... UNTIL, Qualisys thread somehow can't working well if the .join()
+	// function is called OUTSIDE THE IF. WHAT A FUCKING NONSENSE. When I Debug it, there is something wrong with the send() 
+	// function inside RTProtocol Class (a class which govern connection with Qualisys). I can't solve this. 
+	//
+	// Now, because Qualisys somehow requires .join() function within the same block, i can't put it above other code.
+	// It will wait until the Qualisys thread is finished, so that it will block other code. Therefore, i put it below other
+	// code. However, remember i also want a feature that "i can run AMode alone without using Qualisys"? it will be problematic.
+	//
+	// So, i create a this one line block here, instead of in the else{} block from if(useQualisys) below. Everything is
+	// working, and i don't want to touch this part anymore.
+	if (!useQualisys) synch::start();
+
+
+	// create AmodeUSConnection object to connect with A-Mode Machine
+	AModeUSConnection amodeUSConnection(amode_ip, amode_port, amode_mode);
+	// set true if you want to stream data and recording
+	amodeUSConnection.setRecord(true);
+	amodeUSConnection.setDirectory(amode_outputdir);
+	// set true if you also want to recieve index bytes from A-Mode machine (e.g. for indexing data)
+	amodeUSConnection.useDataIndex(true);
+	// create a Amode thread 
+	std::thread threadAMode(std::ref(amodeUSConnection));
+
+
 	if (useQualisys)
 	{
 		// Qualisys is the center of the attention here, it is the class which allows other class to write under its command
@@ -46,39 +81,14 @@ int main()
 		// by specifying command above, streaming mode automatically set to STREAM_USING_COMMAND
 		// however, you can still specify how you want to stream here. for debugging purposes, i used STREAM_USING_NOTHING
 		myQualisysConnection.setStreamingMode(QualisysConnection::STREAM_USING_NOTHING);
-
-		// create a thread
+		// create a thread for Qualisys
 		std::thread threadQualisys(std::ref(myQualisysConnection));
-		// join with all other threads
+		// stupid Qualisys class, i can't put the .join() function outside the if, what a nonsense. (refer to my speech above)
 		threadQualisys.join();
-		printf("here");
-	}
-	else 
-	{
-		// if not using qualisys, directly start the synch
-		synch::start();
 	}
 
-
-	if (useAMode) 
-	{
-		// create AmodeUSConnection object to connect with A-Mode Machine
-		AModeUSConnection amodeUSConnection(amode_ip, amode_port, amode_mode);
-		// set true if you want to stream data and recording
-		amodeUSConnection.setRecord(true);
-		amodeUSConnection.setDirectory(amode_outputdir);
-		// set true if you also want to recieve index bytes from A-Mode machine (e.g. for indexing data)
-		amodeUSConnection.useDataIndex(true);
-
-		// create a thread
-		std::thread threadAMode(std::ref(amodeUSConnection));
-		// join with all other threads
-		threadAMode.join();
-	}
-
-	if (useBMode) {
-		// for the moment i dont put the b-mode
-	}
+	// join with all other threads
+	threadAMode.join();
 
 	return 0;
 }
